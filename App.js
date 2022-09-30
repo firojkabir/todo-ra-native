144794603f5593d4156337bab70a685582776043
firojkabir <firoj.kabir65@gmail.com>
2022-09-30 15:12:32 +0200

    Design list item and add functionality to delete the list

--- App.js
+++ App.js
@@ -1,49 +1,56 @@
-import { useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
+import { useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import Header from "./components/Header";
+import TodoItem from "./components/todoItem";
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    {text: 'grab coffee', key: '1'},
-    {text: 'code to react-app', key: '2'},
-    {text: 'grab another coffee', key: '3'},
-    {text: 'code to react-native-app', key: '4'},
-  ])
+	const [todos, setTodos] = useState([
+		{ text: "grab coffee", key: "1" },
+		{ text: "code to react-app", key: "2" },
+		{ text: "grab another coffee", key: "3" },
+		{ text: "code to react-native-app", key: "4" },
+	]);
 
-  return (
-    <View style={styles.container}>
-      {/* header */}
-      <Header />
-      <View style={styles.content}>
-        {/* to form*/}
-        <View style={styles.list}>
-          <FlatList 
-            data={todos}
-            renderItem={({item}) => (
-              <Text style={styles.text}>{item.text}</Text>
-            )}
-          />
-        </View>
-      </View>
-    </View>
-  );
+	const pressHandler = (key) => {
+		setTodos((prevTodos) => {
+			return prevTodos.filter(todo => todo.key != key);
+		})
+	}
+
+	return (
+		<View style={styles.container}>
+			{/* header */}
+			<Header />
+			<View style={styles.content}>
+				{/* to form*/}
+				<View style={styles.list}>
+					<FlatList
+						data={todos}
+						renderItem={({ item }) => (
+							<TodoItem item={item} pressHandler={pressHandler}/>
+						)}
+					/>
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+	},
 
-  content: {
-    padding: 50,
-  },
+	content: {
+		padding: 50,
+	},
 
-  list: {
-    marginTop: 20,
-  }, 
+	list: {
+		marginTop: 20,
+	},
 
-  text: {
-    fontFamily: 'Comic Sans MS',
-  }
+	text: {
+		fontFamily: "Comic Sans MS",
+	},
 });
